bf6ae098df1bd637145f4e009b91db36978ab335
ARAVINTH0022 <113502178+ARAVINTH0022@users.noreply.github.com>
2026-07-15 15:12:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your code here
+const body = document.querySelector('body');
+
+// Create h1
+const h1 = document.createElement('h1');
+h1.innerText = "Hello World";
+body.appendChild(h1);
+
+// Create h2
+const h2 = document.createElement('h2');
+h2.innerText = "Introducing the Accio Ipsum";
+body.appendChild(h2);
+
+// Create p with the specific Accio ipsum text
+const p = document.createElement('p');
+p.innerText = "Accio ipsum dolor sit amet, join course, solve assignments, get placed above 5 lpa and live life king size.";
+body.appendChild(p);   
